94d15889485b041366357f295c2c4252783ad7b2
C5D2 <yoakec5d2@naver.com>
2024-02-14 14:39:25 +0900

    주접생성기 완성

--- client/main.js
+++ client/main.js
@@ -1,4 +1,15 @@
-import { getNode, getRandom } from "./lib/index.js";
+import {
+  shake,
+  addClass,
+  clearContents,
+  showAlert,
+  getNode,
+  getRandom,
+  insertLast,
+  isNumericString,
+  removeClass,
+  copy,
+} from "./lib/index.js";
 import jujeobData from "./data/data.js";
 
 //  [phase-1]
@@ -14,22 +25,56 @@ import jujeobData from "./data/data.js";
 // - result 내용 비우기(clearContents, node.textContent = "")
 // - pick text => insertAdjacentHTML("beforeend", text), insertLast()
 
+// [phase-2]
+// 1. 아무 값도 입력 받지 못했을 때 예외처리(alert)
+// 2. 공백 문자를 받았을 때 예외처리(alert)
+// - String.prototype.replace + regExp
+// 3. 숫자형 문자를 받았을 때 예외처리(alert)
+
 const submit = getNode("#submit");
 const nameField = getNode("#nameField");
 const result = getNode(".result");
-
-getRandom(6);
 
 function handleJujeob(e) {
   e.preventDefault();
   const name = nameField.value;
   const list = jujeobData(name);
   const pick = list[getRandom(list.length)];
-  console.log(pick);
-  result.textContent = "";
-  result.insertAdjacentHTML("beforeend", pick);
+  //   console.log(pick);
+  //   result.textContent = "";
+  //   result.insertAdjacentHTML("beforeend", pick);
+
+  if (!name || name.replace(/\s*/g, "") === "") {
+    // alert-error에게 is-active 클래스 추가
+    // 2초 뒤에 alert-error에게 is-active 클래스 제거
+    showAlert(".alert-error", "이름을 입력해주세요");
+    shake.restart(); // restart()는 gsap이 가지고 있는 메서드
+    return;
+  }
+
+  // const errorMessage = getNode(".alert-error");
+  // errorMessage.classList.add("is-active");
+  // setTimeout(() => {
+  //   errorMessage.classList.remove("is-active");
+  // }, 2000);
+  if (!isNumericString(name)) {
+    showAlert(".alert-error", "제대로 된 이름을 입력해주세요.");
+    return;
+  }
+
+  clearContents(result);
+  insertLast(result, pick);
+}
+
+function handleCopy() {
+  const text = this.textContent;
+
+  if (!nameField.value) return;
+
+  copy.then(() => {
+    showAlert(".alert-success", "클립보드 복사 완료!!");
+  });
 }
 
 submit.addEventListener("click", handleJujeob);
-
-// [phase-2]
+result.addEventListener("click", handleCopy);
